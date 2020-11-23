5d92fb93af78b901c2bd874a904d8f6bf776b5e7
MichaelSailer <sailer.michi@gmx.at>
2020-11-23 22:50:52 +0100

    Login, Register, Startseite, Update und Design

--- goddamitmichi/js/controller.js
+++ goddamitmichi/js/controller.js
@@ -21,4 +21,79 @@ function addNewJob(){
 //Zurück zur Hauptseite wechseln
 
 
-//
+//Check Login
+
+ function checkLogin(){
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    
+    
+    if(keyForLogin(email,password)){
+        window.location = "index.html"
+    }
+}
+
+ function datenLaden(){
+    let key = localStorage.getItem("schlüssel");
+    let userInfo =  getUserByKey(key);
+
+}
+function goToRegister(){
+    console.log("Test");
+    window.location = "Register.html";
+}
+function goToLogin(){
+    window.location = "Login.html"
+
+}
+function goBack(){
+    window.location = "index.html"
+}
+function signOut(){
+    localStorage.removeItem("schlüssel");
+    window.location = "Login.html";
+}
+function saveUser(){
+    let firstName=document.getElementById("firstname").value;
+    let lastName=document.getElementById("lastname").value;
+    let email=document.getElementById("email").value;
+    let password=document.getElementById("password").value;
+    let user={firstName:firstName, lastName:lastName, email:email, password:password};
+    
+    keyForRegister(user);
+
+    
+}
+
+function test(itemKey){
+    localStorage.setItem("updateItem",itemKey);
+    window.location = "updateTask.html";
+}
+
+function makeHtml(description, title, until, schlüssel){
+    let htmlString =  '<button onclick=test("'+schlüssel+'") class="list-group-item list-group-item-action bg-secondary">'
+    htmlString += '<div class="d-flex w-100 justify-content-between">'
+    htmlString += '<h5 class="mb-3 text-light">'+title+'</h5>'
+    htmlString += '<small class="text-white" >Until: '+until+'</small></div>'
+    
+    htmlString += '<p class="mb-1 text-white">'+description+'</p>'
+    htmlString += '</button>'
+
+    document.getElementById("toDoListe").innerHTML += htmlString;
+}
+
+function loadData(){
+    let key = localStorage.getItem("updateItem");
+    getTaskByKey(key);
+
+}
+
+function loadIndexSite(){
+    getData();
+}
+
+function deleteTaskFromUI(){
+    deleteTask(localStorage.getItem("updateItem"));
+    alert("Congratulation you did a nice Job!");
+    window.location = "index.html"    
+}
--- goddamitmichi/js/database.js
+++ goddamitmichi/js/database.js
@@ -22,6 +22,7 @@
 // Initialisierung der Datenbank. Hier wird der Hauptdatenbank angesprochen.
 var db = firebase.database().ref();
 var userdb = firebase.database().ref().child("users");
+var taskdb = {}
 
 //So sollte der JSON aussehen:
 let TaskJSON = {
@@ -34,7 +35,8 @@ let TaskJSON = {
 let userJSON = {
     firstName: "Max",
     lastName: "Mustermann",
-    email: "max.mustermann@gmail.com"
+    email: "max.mustermann@gmail.com",
+    password: "max"
 }
 
 
@@ -45,7 +47,7 @@ let userJSON = {
 // Neuen User einfügen: 
 // wird bei Regristrien von neuen User aufgerufen nur mit richtigen JSON Daten
 // Funktion newUser (vorname, nachname, email);
-    userdb.push(userJSON);
+    //userdb.push(userJSON);
 
 // alle  User auslesen:
 
@@ -54,76 +56,116 @@ let keysFromUsers = [];
 
 //Holt sich alle Daten aus userdb raus. Daher wir push verwendetet wird für jeden Eintrag ein Key generiert
 //und an diesen Schlüssel hängt dann unsere Daten (Name, Email,...)
-userdb.on("value", snap => {
-    let values = snap.val();
-    //Object.keys() holt sich den Schlüssel von den Eintrag
-    keysFromUsers = Object.keys(values);
-    gethThrougAllUsers()
-});
+
 
 
 //Diese Funktion geht durch alle Schlüssel durch und schaut in den einzelnen Einträge nach und gibt
 //die Daten zu diesen Schlüssel aus.
-function getThrougAllUsers(){
 
-    keysFromUsers.forEach(schlüssel => {
-        userdb.child(schlüssel).on("value", snap => {
-            let values = snap.val();
-            console.log(values.email);
-        })
+
+
+
+function keyForLogin(email,password){
+    let keys = [];
+    let loginErfolgreich = false
+    userdb.on('value', (snap) => {
+        keys = Object.keys(snap.val());
+    })
+
+    keys.forEach((val) => {
+        if(checkUserforLogin(val,email,password)){
+            localStorage.setItem("schlüssel", val);
+            loginErfolgreich = true
+            taskdb = userdb.child(val)
+        }
+    })
+    return loginErfolgreich;
+}
+
+function checkUserforLogin(key,email,password){
+    let value
+    let erfolgreich = false
+    userdb.child(key).on('value',snap=>{
+        value = snap.val();
+        
     });
+    if(value.email == email && value.password == password){
+        erfolgreich = true;
+    }
+    return erfolgreich;
+}
+
+function keyForRegister(user){
+    let keys = [];
+    let registerErfolgreich = false
+    let schlüssel = null;
+    userdb.on('value', (snap) => {
+        keys = Object.keys(snap.val());
+    })
+
+    keys.forEach((val) => {
+        schlüssel =  checkUserforRegister(val,user)
+        
+    })
+    if(schlüssel) {
+        userdb.push(user);
+        localStorage.setItem("schlüssel", schlüssel);
+        registerErfolgreich = true;
+        taskdb = userdb.child(schlüssel)
+        window.location = "index.html";
+    }else{
+        alert("Die E-Mail Adresse wurde schon verwendet");
+    }
+    return registerErfolgreich;
+}
+
+function checkUserforRegister(key,user){
+    let registerOkay = key;
+    let dbUser = {}
+    userdb.child(key).on('value',snap=>{
+        dbUser = snap.val();
+        
+    });
+    if(user.email == dbUser.email){
+        registerOkay = null;
+    }
+    return registerOkay;
+}
+function getUserByKey(key){
+    let user = {}
+    userdb.child(key).on('value',snap => {
+        //datenAnzeigen(snap.val());
+        
+    })
 
 }
+
 
 
 //Elena und Nicole probieren USER
 //Erzeugen
 
-userdb.on('child_added', snap => {
-    let item=snap.val();
-    document.getElementById("content").innerHTML+='<div id="'+snap.key+'">'+item.name+'<a href="#" onclick="deleteUser(\''+snap.key+'\')">Löschen</a></div><br/>';
-});
-
-function saveUser(){
-    let firstName=document.getElementById("firstName").value;
-    let lastName=document.getElementById("lastName").value;
-    let email=document.getElementById("email").value;
-    let user={firstName:firstName, lastName:lastName, email:email};
-    userdb.push(user);
-    //            function(error){
-    //    if(error){
-    //        alert("Beim Speichern der Daten ist ein Fehler aufgetreten. Bitte wenden Sie sich an jemand");
-    //    }/*else{
-    //        document.getElementById("")
-    //    }*/
-    //}
-}
-
-//Löschen
-userdb.on('child_removed', snap=>{
-   document.getElementById(snap.key).remove(); 
-});
-
-function deleteUser(key){
-    userdb.child(key).remove();
-}
-
 
 //Elena und Nicole probieren TODO
 //Erzeugen
-
-db.on('child_added', snap => {
-    let item=snap.val();
-    document.getElementById("content").innerHTML+='<div id="'+snap.key+'">'+item.name+'<a href="#" onclick="deleteTask(\''+snap.key+'\')">Löschen</a></div><br/>';
-});
+function getData(){
+    userdb.child(localStorage.getItem("schlüssel")).on('child_added', snap => {
+        let item=snap.val();
+        if(typeof(item) != "string"){
+            makeHtml(item.taskDescription, item.taskTitle, item.until, snap.key);
+        }
+        //document.getElementById("content").innerHTML+='<div id="'+snap.key+'">'+item.name+'<a href="#" onclick="deleteTask(\''+snap.key+'\')">Löschen</a></div><br/>';
+    });
+}
 
 function saveTask(){
     let taskTitle=document.getElementById("taskTitle").value;
     let taskDescription=document.getElementById("taskDescription").value;
     let until=document.getElementById("until").value;
-    let time=document.getElementById("time").value;
-    let user={taskTitle:taskTitle, taskDescription:taskDescription, until:until, time:time};
-    db.push(task);
+    let task={taskTitle:taskTitle, taskDescription:taskDescription, until:until};
+    taskdb = userdb.child(localStorage.getItem("schlüssel"));
+    taskdb.push(task);
+    window.location = "index.html"
     //            function(error){
     //    if(error){
     //        alert("Beim Speichern der Daten ist ein Fehler aufgetreten. Bitte wenden Sie sich an jemand");
@@ -139,12 +181,17 @@ db.on('child_removed', snap=>{
 });
 
 function deleteTask(key){
-    db.child(key).remove();
+    taskdb = userdb.child(localStorage.getItem("schlüssel"));
+    taskdb.child(key).remove();
 }
 
 
-
-    
-
-
-
+function getTaskByKey(key){
+    taskdb = userdb.child(localStorage.getItem("schlüssel"));   
+    taskdb.child(key).on('value',snap=>{
+        let item = snap.val();
+        document.getElementById("taskTitle").value = item.taskTitle
+        document.getElementById("taskDescription").value = item.taskDescription
+        document.getElementById("until").value = item.until
+    });
+}
